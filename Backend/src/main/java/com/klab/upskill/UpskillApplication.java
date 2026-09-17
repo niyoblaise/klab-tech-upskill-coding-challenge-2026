06a506a89a849bdc4eafa85312e41cf6f0764207
Niyonshuti <niyoblaise7@gmail.com>
2026-09-17 11:25:57 +0200

    chore: initialize backend project

---
+++ Backend/src/main/java/com/klab/upskill/UpskillApplication.java
@@ -0,0 +1,13 @@
+package com.klab.upskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpskillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpskillApplication.class, args);
+	}
+
+}
