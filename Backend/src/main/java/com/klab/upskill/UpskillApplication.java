c4a68583cedab11cfaf47f5fea70f54eb9582443
Niyonshuti <niyoblaise7@gmail.com>
2026-09-17 20:58:46 +0200

    starter class

--- Backend/src/main/java/com/klab/upskill/UpskillApplication.java
+++ Backend/src/main/java/com/klab/upskill/UpskillApplication.java
@@ -2,8 +2,10 @@ package com.klab.upskill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class UpskillApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class UpskillApplication {
 	}
 
 }
+
